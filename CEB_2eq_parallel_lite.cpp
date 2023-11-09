89a178e205e29d58ca7705e436e9ff4a8e4102bd
macPimanov <macpimanov@gmail.com>
2023-11-09 12:58:59 +0300

    Update CEB_2eq_parallel_lite.cpp, some comments + formatting

--- CEB_2eq_parallel_lite.cpp
+++ CEB_2eq_parallel_lite.cpp
@@ -3,16 +3,15 @@
 
 //for bolometers (absorber + 2SINs) w/ Andreev Current
 
-
 //#include <complex.h>
 #include "header.h"
 
 #define PI 3.14159265358979
-#define ME 9.10938188e-31 //kg
-#define EL 1.60217646e-19 //coulombs
-#define HPLANCK 6.626068e-34 //m2 kg / s
-#define HBAR 1.05457148e-34 //m2 kg / s
-#define KBOL 8.6173324e-5 //eV K-1
+#define ME 9.10938188e-31	//kg
+#define EL 1.60217646e-19	//coulombs
+#define HPLANCK 6.626068e-34	//m2 kg / s
+#define HBAR 1.05457148e-34	//m2 kg / s
+#define KBOL 8.6173324e-5	//eV K-1
 #define THREADS 56
 
 //1 electron volt = 1.60217646e-19 joule
