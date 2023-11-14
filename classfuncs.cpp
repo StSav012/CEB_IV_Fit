bd016135eaca1bb61623d7260ea994ef010e4972
macPimanov <macpimanov@gmail.com>
2023-11-14 18:35:10 +0300

    Update classfuncs.cpp

--- classfuncs.cpp
+++ classfuncs.cpp
@@ -36,41 +36,41 @@ CFoo::CFoo(int parnum)
 	}
 	fclose(parfile);
 
-	par[0]=param[0]; Pbg=par[0]; ToFit[0]=Fit[0]; //power for all structure
+	par[0]=param[0]; Pbg=par[0]; ToFit[0]=Fit[0];		//power for all structure
 
-	par[1]=param[1]; beta=par[1]; ToFit[1]=Fit[1];//returned power
+	par[1]=param[1]; beta=par[1]; ToFit[1]=Fit[1];		//returned power
 
-	par[2]=param[2]; TephPOW=par[2]; ToFit[2]=Fit[2]; //returned power
+	par[2]=param[2]; TephPOW=par[2]; ToFit[2]=Fit[2];	//exponent for Te-ph
 
-	par[3]=param[3]; gamma=par[3]; ToFit[3]=Fit[3]; //gap smearing, returned power
+	par[3]=param[3]; gamma=par[3]; ToFit[3]=Fit[3];		//gap smearing
 
-	par[4]=param[4]; Vol=par[4]; ToFit[4]=Fit[4]; //volume of absorber in um^3
+	par[4]=param[4]; Vol=par[4]; ToFit[4]=Fit[4];		//volume of absorber
 
-	par[5]=param[5]; VolS=par[5]; ToFit[5]=Fit[5]; //volume of superconductor
+	par[5]=param[5]; VolS=par[5]; ToFit[5]=Fit[5];		//volume of superconductor
 
-	par[6]=param[6]; Z=par[6]; ToFit[6]=Fit[6]; // heat exchange in normal metal in nW/(K^5*micron^3)
+	par[6]=param[6]; Z=par[6]; ToFit[6]=Fit[6];		//heat exchange in normal metal
 
-	par[7]=param[7]; ZS=par[7]; ToFit[7]=Fit[7]; //sigma and volume of supercond-r
+	par[7]=param[7]; ZS=par[7]; ToFit[7]=Fit[7];		//sigma of superconductor
 
-	par[8]=param[8]; Tc=par[8]; ToFit[8]=Fit[8]; //critical temp of superconductor
+	par[8]=param[8]; Tc=par[8]; ToFit[8]=Fit[8];		//critical temperature of superconductor
 
-	par[9]=param[9]; Rn=par[9]; ToFit[9]=Fit[9]; //total normal resistance, Ohm 
+	par[9]=param[9]; Rn=par[9]; ToFit[9]=Fit[9];		//total normal resistance
 
-	par[10]=param[10]; Rleak=par[10]; ToFit[10]=Fit[10]; //leakage, Ohm
+	par[10]=param[10]; Rleak=par[10]; ToFit[10]=Fit[10];	//leakage resistance
 
-	par[11]=param[11]; Wt=par[11]; ToFit[11]=Fit[11]; //transparency of barter
+	par[11]=param[11]; Wt=par[11]; ToFit[11]=Fit[11];	//transparency of barter
 
-	par[12]=param[12]; tm=par[12]; ToFit[12]=Fit[12]; //depairing energy
+	par[12]=param[12]; tm=par[12]; ToFit[12]=Fit[12];	//depairing energy
 
-	par[13]=param[13]; ii=par[13]; ToFit[13]=Fit[13]; //coefficient for Andreev current
+	par[13]=param[13]; ii=par[13]; ToFit[13]=Fit[13];	//coefficient for Andreev current
 
-	par[14]=param[14]; Ra=par[14]; ToFit[14]=Fit[14]; //resistance of absorber, Ohm
+	par[14]=param[14]; Ra=par[14]; ToFit[14]=Fit[14];	//resistance of absorber
 	
-	par[15]=param[15]; M=par[15]; ToFit[15]=Fit[15]; //number of bolometers in series,
+	par[15]=param[15]; M=par[15]; ToFit[15]=Fit[15];	//number of bolometers in series
 
-	par[16]=param[16]; MP=par[16]; ToFit[16]=Fit[16]; // number of bolometers in parallel
+	par[16]=param[16]; MP=par[16]; ToFit[16]=Fit[16];	//number of bolometers in parallel
 
-	par[17]=param[17]; Tp=par[17]; ToFit[17]=Fit[17]; //phonon temperature
+	par[17]=param[17]; Tp=par[17]; ToFit[17]=Fit[17];	//phonon temperature
 
 	par[18]=param[18]; dVFinVg=par[18]; ToFit[18]=Fit[18];
 	
@@ -78,10 +78,10 @@ CFoo::CFoo(int parnum)
 	
 	par[20]=param[20]; dV=par[20]; ToFit[20]=Fit[20];
 
-	char fname[]="SINS1_53_240_303_mK.txt";  // default filename here. Format: two columns (voltage (V), current(A)), decimal delimiter: .
+	char fname[]="SINS1_53_240_303_mK.txt";			//default filename here. Format: two columns (voltage (V), current(A)), decimal delimiter: .
 	
-	//printf("Enter the name of file: \n"); //example: "ol g7-25nn\200mk_2g.txt"
-	//scanf("%[^\n]", fname); //your filename here	Iexp=NULL;
+	//printf("Enter the name of file: \n");			//example: "ol g7-25nn\200mk_2g.txt"
+	//scanf("%[^\n]", fname);				//your filename here	Iexp=NULL;
 	
 	Vexp=NULL;
 	Inum=NULL;
