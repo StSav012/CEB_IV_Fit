261d70675db49ceb927355ea7ab373ef62615118
StSav012 <stsav012@gmail.com>
2023-12-09 19:36:02 +0300

    Enumerate the parameters of `startparams.txt`

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # CEB_IV_Fit
 A complicated program for automatic fitting of CEB IV-curves.
 It needs an IV-curve *.txt file and a file with start parameters.
+
+#### Parameters in `startparams.txt`:
+0. incident power for all structure [pW]
+1. returned power ratio, < 1
+2. exponent for Te-ph
+3. (unused) gap smearing
+4. volume of absorber [um³]
+5. (unused) volume of superconductor [um³]
+6. heat exchange in normal metal [nW/(K⁵×um³)]
+7. (unused) sigma of superconductor
+8. critical temperature of the superconductor [K]
+9. total normal resistance of all bolometers [Ohm]
+10. total leakage resistance of all bolometers [Ohm]
+11. transparency of the barrier
+12. depairing energy
+13. coefficient for Andreev current
+14. normal resistance of 1 absorber [Ohm]
+15. number of bolometers in series
+16. number of bolometers in parallel
+17. phonon temperature [K]
+18. upper voltage limit relative to the gap
+19. lower voltage limit relative to the gap
+20. voltage step relative to the gap
--- constants.h
+++ constants.h
@@ -1,14 +1,63 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
-#define PI 3.14159265358979
-#define ME 9.10938188e-31    //kg
-#define EL 1.60217646e-19    //coulombs
-#define HPLANCK 6.626068e-34 //m2 kg / s
-#define HBAR 1.05457148e-34  //m2 kg / s
-#define KBOL 8.6173324e-5    //eV K-1
+#include <cmath>
+
+/*
+ * Ensure the math constants are defined one way or another
+ */
+#ifndef M_PI
+const double M_PI = std::atan2(1.0, 1.0) * 4.0;
+#endif
+
+#ifndef M_SQRT2
+const double M_SQRT2 = std::sqrt(2.0);
+#endif
+
+#ifndef M_SQRT1_2
+const double M_SQRT1_2 = 1.0 / M_SQRT2;
+#endif
+
+/*
+ * 2018 CODATA recommended values
+ *
+ * see https://physics.nist.gov/cuu/Constants/index.html
+ */
+const double ME = 9.1093837015e-31;         // ± 0.0000000028e-31, [kg]
+const double EL = 1.602176634e-19;          // exact,              [C]
+const double HPLANCK = 6.62607015e-34;      // exact,              [J / Hz]
+const double HBAR = HPLANCK / (2.0 * M_PI); //                     [J / Hz]
+const double KBOL = 1.380649e-23 / EL;      // exact,              [eV / K]
+
 #define THREADS 56
 
-//1 electron volt = 1.60217646e-19 joule
+/*
+ * The scales to use when calculating integrals
+ *
+ * The variable to integrate over makes the steps of this size
+ * relative to a characteristic value.
+ */
+// the step of an integral calculation
+const double INTEGRATION_SCALE = 0.2e-3;
+// the large enough value
+const double ESSENTIALLY_INFINITY_SCALE = 20.0;
+
+/*
+ * Voltage and current noise for some amplifiers
+ */
+const double vn = (3.2e-9) * M_SQRT2;    //V/sqrt(Hz), for 2 amp * sqrt(2) AD745 amplifier
+const double in = (6.9e-15) * M_SQRT1_2; //A/sqrt(Hz), for 2 amp / sqrt(2)
+
+//		vn = (8.0e-9) * M_SQRT2;					//V/sqrt(Hz), for 2 amp * sqrt(2) OPA111 amplifier
+//		in = (0.8e-15) * M_SQRT1_2;					//A/sqrt(Hz), for 2 amp / sqrt(2)
+
+//		vn = (0.9e-9) * M_SQRT2;					//V/sqrt(Hz), for 2 amp * sqrt(2) AD797 amplifier
+//		in = (2.0e-12) * M_SQRT1_2;					//A/sqrt(Hz), for 2 amp / sqrt(2)
+
+//		vn = (1.1e-9) * M_SQRT2;					//V/sqrt(Hz), for 2 amp * sqrt(2) IFN146 amplifier
+//		in = (0.3e-15) * M_SQRT1_2;					//A/sqrt(Hz), for 2 amp / sqrt(2)
+
+//		vn = (5.1e-9) * M_SQRT2;					//V/sqrt(Hz), for 2 amp * sqrt(2) OPA1641 amplifier
+//		in = (0.8e-15) * M_SQRT1_2;					//A/sqrt(Hz), for 2 amp / sqrt(2)
 
 #endif
